5a0b78c73965827b7c0e6186e46008b2186c3051
Anurag Yadav <yanurag994@gmail.com>
2024-09-03 15:31:21 -0400

    Testing JWT based Auth for Tableau

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+import jwt, uuid , datetime #Interatcion with Tableau
+import hashlib #User Password Management
+
+import os
 
 app = Flask(__name__)
 
+connectedAppClientId = os.environ['CONNECTED_APP_CLIENT_ID']
+connectedAppSecretId = os.environ['CONNECTED_APP_SECRET_ID']
+connectedAppSecretKey = os.environ['CONNECTED_APP_SECRET_KEY']
+user = os.environ['TABLEAU_USER']
+
+def get_token():
+    return jwt.encode(
+        {
+            "iss": connectedAppClientId,
+            "exp": datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=30),
+            "jti": str(uuid.uuid4()),
+            "aud": "tableau",
+            "sub": user,
+            "scp": ["tableau:views:embed", "tableau:metrics:embed"],
+            "Region": "East"
+        },
+        connectedAppSecretKey,
+        algorithm="HS256",
+        headers={
+            'kid': connectedAppSecretId,
+            'iss': connectedAppClientId
+        }
+    )
 
 @app.route('/login')
 def login():
-    return render_template("index.html")
+    return render_template("index.html", jwt=get_token())
 
 @app.route('/register')
 def register():
